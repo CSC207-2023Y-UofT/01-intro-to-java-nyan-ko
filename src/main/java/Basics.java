a61c99201751feba2d718efaec5bbb143f7fa76c
nyan-ko <dumbstupidcatboy@gmail.com>
2023-06-11 11:50:09 -0400

    Fix task 3

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -102,7 +102,7 @@ public class Basics {
          */
         for (int i = 10; i >= 0; i--)
         {
-            System.out.println(i);
+            System.out.println("Current count: " + i);
         }
     }
 
